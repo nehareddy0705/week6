07f08a87528bce626123cd727524badad2aef81a
nehareddy0705 <mittabaineha0705@gmail.com>
2026-05-13 15:32:29 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "http://localhost:5176"
+      "https://week6-blush.vercel.app/create-emp"
     ]
   })
 );
